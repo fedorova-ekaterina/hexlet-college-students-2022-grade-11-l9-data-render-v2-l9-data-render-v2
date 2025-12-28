5e3cc41211860405aa437bc303bd222ab6a56aaf
ekaterina fedorova <fedorova.ekaterina@hexly.ru>
2025-12-28 17:36:04 +0300

    1

--- src/CafesTable.jsx
+++ src/CafesTable.jsx
@@ -1,1 +1,16 @@
+jsx
+import React from 'react';
+import FilterCafes from './FilterCafes';
 
+const CafesTable = () => {
+  return (
+    <div id="container" className="container m-3">
+      <div className="cafesTable">
+        <FilterCafes />
+        <ul className="cardsList"></ul>
+      </div>
+    </div>
+  );
+};
+
+export default CafesTable;
--- src/FilterCafes.jsx
+++ src/FilterCafes.jsx
@@ -1,1 +1,48 @@
+jsx
+import React from 'react';
 
+const FilterCafes = ({ onFilterChange }) => {
+  const subwayOptions = [
+    {
+      name: "Арбатская",
+      code: "Arbat",
+    },
+    {
+      name: "Александровский сад",
+      code: "Alexanders Garden",
+    },
+    {
+      name: "Московская",
+      code: "Moscow",
+    },
+    {
+      name: "Парк Культуры",
+      code: "Culture",
+    },
+    {
+      name: "Театральная",
+      code: "Theater",
+    },
+  ];
+
+  const handleChange = (e) => {
+    if (onFilterChange) {
+      onFilterChange(e.target.value);
+    }
+  };
+
+  return (
+    <div className="controls">
+      <select name="subway" id="subway" onChange={handleChange}>
+        <option value="All">Все</option>
+        {subwayOptions.map((option) => (
+          <option key={option.code} value={option.code}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default FilterCafes;
